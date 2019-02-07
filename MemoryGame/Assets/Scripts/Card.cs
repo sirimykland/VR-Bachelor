664db4b99f44429783a5d1b9dc746c22d9533afd
sirimykland <35257015+sirimykland@users.noreply.github.com>
2019-02-07 08:42:04 +0100

    07.02.2019

--- MemoryGame/Assets/Scripts/Card.cs
+++ MemoryGame/Assets/Scripts/Card.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+
+
 
 public class Card : MonoBehaviour {
 
@@ -15,31 +16,58 @@ public class Card : MonoBehaviour {
     [SerializeField]
     private bool _initialized = false;
 
-
-    //private GameObject manager;
-    //private GameObject m_instance; // reference to card when created
-
+    private float _speed = 10F;
     void Start(){
-        _state = 1;
-        //manager = GameObject.FindGameObjectWithTag("Manager");
+        _state = 0;
     }
 
+    IEnumerator rotateStuff()
+    {
+        float t = 0;
+        float root = transform.eulerAngles.y;
+
+        float target = transform.eulerAngles.y+ 180;
+
+        if (root > target)
+        {
+            root= target;
+            target = 0;
+        }
+
+
+        while (root < target)
+        {
+            t += Time.deltaTime;
+            root = Mathf.Lerp(root, target, t * 0.5f);
+            transform.rotation = Quaternion.Euler(0, root, 0);
+            yield return null;
+        }
+    }
+    
+
+    void OnMouseUpAsButton() { 
+            //Debug.Log("Pressed card.");
+            FlipCard();
+    }
     public void SetupGraphics(Material backside) {
-        gameObject.GetComponentInChildren<Renderer>().material = new Material(backside);
-        FlipCard();
+        this.gameObject.GetComponentInChildren<Renderer>().material = new Material(backside);
     }
 
     public void FlipCard() {
-
-        if (_state == 0)
+        if (_state == 0){
             _state = 1;
-        else if (_state == 1)
+        }else if (_state == 1){
             _state = 0;
-               
-        if (_state == 0 && !DO_NOT_TURN)
-            gameObject.transform.Rotate(0,0,0);
-        else if (_state == 1 && !DO_NOT_TURN)
-            gameObject.transform.Rotate(Vector3.up, 180);
+        }
+
+        if (_state == 0 && !DO_NOT_TURN){
+            StartCoroutine(rotateStuff());        
+        }
+        else if (_state == 1 && !DO_NOT_TURN){
+            StartCoroutine(rotateStuff());
+        }
+
+        //Debug.Log("Flipped card"+ _state);
     }
 
     public int CardValue{
@@ -69,10 +97,10 @@ public class Card : MonoBehaviour {
     IEnumerator pause() {
         yield return new WaitForSeconds(1);
         if (_state == 0){
-            gameObject.transform.Rotate(0, 180, 0);
-            Debug.Log("Pressed button.");
+            StartCoroutine(rotateStuff());
+            
         } else if (_state == 1) {
-            gameObject.transform.Rotate(0, 0, 0);
+           StartCoroutine(rotateStuff());
         }
         DO_NOT_TURN = false;
     }
--- MemoryGame/Assets/Scripts/GameManager.cs
+++ MemoryGame/Assets/Scripts/GameManager.cs
@@ -8,7 +8,6 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     public Material[] backsides;
-    
 
     public Card[] cards;
     public Text matchText;
@@ -29,7 +28,7 @@ public class GameManager : MonoBehaviour {
             InitializeCards();
 
         if (Input.GetMouseButtonUp(0)) {
-            Debug.Log("Pressed button.");
+            //Debug.Log("Pressed button.");
             CheckCards();
         }
     }
@@ -39,7 +38,7 @@ public class GameManager : MonoBehaviour {
         m_StartWait = new WaitForSeconds(m_StartDelay);
         m_EndWait = new WaitForSeconds(m_EndDelay);
 
-        for (int i = 1; i < 9; i++)
+        for (int i = 0; i < 8; i++)
         {
             for (int j = 0; j < 2; j++)
             {
@@ -49,7 +48,7 @@ public class GameManager : MonoBehaviour {
                 while (!test)
                 {
                     choice = UnityEngine.Random.Range(0, cards.Length);
-                    Debug.Log(choice);
+                    //Debug.Log(choice);
                     test = !(cards[choice].Initialized);
                 }
                 cards[choice].CardValue = i;
@@ -59,50 +58,50 @@ public class GameManager : MonoBehaviour {
         }
 
         foreach (Card c in cards) { 
-            c.SetupGraphics(backsides[c.CardValue*2+ c.CardType]);
+            c.SetupGraphics(backsides[(c.CardValue + c.CardValue + c.CardType)]);
+            //Debug.Log("card no: " + (c.CardValue + c.CardValue + c.CardType) );
         }
 
         if (!_init)
             _init = true;
 
-        Debug.Log("cards initialized");
+        //Debug.Log("cards initialized");
     }
 
-    public Material GetBackside(int i)
-    {
+    public Material GetBackside(int i){
         return backsides[i - 1];
     }
 
     void CheckCards(){
         List<int> c = new List<int>();
-        Debug.Log("Checking cards...");
+        //Debug.Log("Checking cards...");
         for(int i=0; i< cards.Length; i++)
         {
             if (cards[i].State == 1)
             {
                 c.Add(i);
-                Debug.Log("Card no " + cards[i].CardValue + "was clicked."+ cards[i].State);
+                //Debug.Log("Card no " + cards[i].CardValue + "was clicked."+ cards[i].State);
             }
         }
 
         if (c.Count == 2)
         {
-            Debug.Log("two cards chosen");
+            //Debug.Log("two cards chosen");
             CardComparison(c);
         }
-        Debug.Log("cards have been checked");
+        //Debug.Log("cards have been checked");
     }
     void CardComparison(List<int> c)
     {
         Card.DO_NOT_TURN = true;
 
         int x = 0;
-        if(cards[c[0]].CardValue ==  cards[c[1]].CardValue)
+        if(cards[c[0]].CardValue ==  cards[c[1]].CardValue && cards[c[0]].CardType != cards[c[1]].CardType)
         {
             x = 2;
             matches--;
             matchText.text = "Number of matching pairs left: "+matches;
-            Debug.Log("Cards matches.");
+            //Debug.Log("Cards matches.");
             if (matches == 0)
                 SceneManager.LoadScene("GameMenu");
             
