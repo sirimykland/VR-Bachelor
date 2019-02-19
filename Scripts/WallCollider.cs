847d80cedba7de5ea22366b7319665e89146d599
Sebastian Sonesen <35290401+sebsone@users.noreply.github.com>
2019-02-19 13:34:54 +0100

    First commit

---
+++ Scripts/WallCollider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : MonoBehaviour {
+
+    //Connects script with the script AtomCollider to get its public variables
+    public GameBehaviour gameBehaviour;
+
+    public AudioClip positiveWallSound;
+    public AudioClip negativeWallSound;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider atom)
+    {
+        if ((!gameBehaviour.gameOver) && atom.gameObject.CompareTag("NonMetal"))
+        {
+            source.PlayOneShot(negativeWallSound, 1f);
+
+            gameBehaviour.lives--;
+            gameBehaviour.livesText.text = "Lives: " + gameBehaviour.lives.ToString();
+            Destroy(atom.gameObject);
+        }
+        
+        if (atom.gameObject.CompareTag("Metal"))
+        {
+            source.PlayOneShot(positiveWallSound, 1f);
+            Destroy(atom.gameObject);
+        }
+    }
+}
