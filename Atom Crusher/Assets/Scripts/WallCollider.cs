c03f7d464b55975d89c2c0c750a615d05959b213
sebson <ssebsson@gmail.com>
2019-03-04 13:06:29 +0100

    Revert "delete atom crusher from branch"
    This reverts commit 7fd3cc2e470398949925c2b38d4fb5ca6b7140ac.

---
+++ Atom Crusher/Assets/Scripts/WallCollider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : MonoBehaviour {
+
+    //Connects script with the script AtomCollider to get its public variables
+    public GameBehaviour gameBehaviour;
+
+    public AudioClip positiveWallSound;
+    public AudioClip negativeWallSound;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider atom)
+    {
+        if ((!gameBehaviour.gameOver) && atom.gameObject.CompareTag("NonMetal"))
+        {
+            source.PlayOneShot(negativeWallSound, 0.4f);
+
+            gameBehaviour.lives--;
+            gameBehaviour.livesText.text = "Lives: " + gameBehaviour.lives.ToString();
+            Destroy(atom.gameObject);
+        }
+        
+        if (atom.gameObject.CompareTag("Metal"))
+        {
+            source.PlayOneShot(positiveWallSound, 0.4f);
+            Destroy(atom.gameObject);
+        }
+    }
+}
